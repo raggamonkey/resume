18f8d982596c5e5885e3d095f3fd1f42d9b30f8f
raggamonkey <154525023+raggamonkey@users.noreply.github.com>
2025-11-26 19:51:34 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', // generate dark: variants used by your page
-  content: ["./index.html", "./src/**/*.{html,js}"], // include all html/js so classes are not purged
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
