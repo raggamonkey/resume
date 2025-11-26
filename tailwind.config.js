627111db8ea22df4317585403f20dd13f8353e42
raggamonkey <154525023+raggamonkey@users.noreply.github.com>
2025-11-26 14:54:59 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // adjust this if your HTML files are in subfolders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
