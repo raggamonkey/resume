a6cb6a07f9af7a7e11762a809d359d01f78e84e6
raggamonkey <154525023+raggamonkey@users.noreply.github.com>
2025-11-26 17:01:40 +0100

    Update content path in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"], // adjust this if your HTML files are in subfolders
+  content: ["./index.html"], // adjust this if your HTML files are in subfolders
   theme: {
     extend: {},
   },
   plugins: [],
+
 }
